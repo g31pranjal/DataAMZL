256d58a28d658e6c645e2d848aab3892dd394cdb
yash512 <yashtyagi512@gmail.com>
2017-08-17 01:08:33 +0530

    pranjal ke muh mei

--- AMZL/settings.py
+++ AMZL/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Visual.apps.VisualConfig',   
+    'Visual.apps.VisualConfig',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
